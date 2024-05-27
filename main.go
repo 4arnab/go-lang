234ac64b8ed431d61fadd8c04461bd9677f40dc9
4arnab <axmedarnab3@gmail.com>
2024-05-27 12:00:08 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Var conferance
+	var conferanceName = "Go Conferance"
+	const conferaneTickets = 50
+
+	fmt.Println("Welcome to our", conferanceName, "booking Application")
+	getTicketMessage := "Get your ticket here to attend"
+	fmt.Println(getTicketMessage)
+
+}
